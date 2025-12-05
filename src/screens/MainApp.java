dbaf4359801fbf2bd9d73ca46893d192703b08e5
sommie <valkvng@gmail.com>
2025-12-05 00:06:34 +0000

    Started working on screens package, updated main app and added courses view file, configured javafx to start running

---
+++ src/screens/MainApp.java
@@ -0,0 +1,25 @@
+package screens;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("CoursesView.fxml")
+        );
+
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Course Registration System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
