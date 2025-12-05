c26997c1d93d2b573b98e86fe6a751cca3f2a611
sommie <valkvng@gmail.com>
2025-12-05 00:06:34 +0000

    Started working on screens package, updated main app and added courses view file, configured javafx to start running

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.assignment {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.assignment to javafx.fxml;
-    exports com.example.assignment;
-}
